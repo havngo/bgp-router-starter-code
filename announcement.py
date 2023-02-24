d486a2831dfaecc3d17d4b3495589eded285f795
Octillerysnacker <octillerysnacker@gmail.com>
2023-02-23 19:39:21 -0500

    completed steps 2 and 3

--- announcement.py
+++ announcement.py
@@ -1,3 +1,4 @@
+import json
 from typing import List
 
 
@@ -10,3 +11,16 @@ class Announcement:
         self.ASPath = ASPath
         self.selfOrigin = selfOrigin
         self.origin = origin
+    def __str__(self) -> str:
+        return json.dumps({"peer": self.peer, "netmask": self.netmask, "network": self.network})
+    def __repr__(self) -> str:
+        return self.__str__()
+    def getOriginLevel(self):
+        if self.origin == "IGP":
+            return 2
+        elif self.origin == "EGP":
+            return 1
+        elif self.origin == "UNK":
+            return 0
+        else:
+            return -1
